05f45a0a96c77beed3eaed75f19b0a93f10c7c12
mod washi <your.email@example.com>
2013-03-07 16:19:48 +0530

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 DrawApp::Application.routes.draw do
-  get "static_pages/home"
+  resources :users
+   resources :sessions, only: [:new, :create, :destroy]
 
-  get "static_pages/help"
-
-  get "static_pages/about"
+    root to: 'static_pages#home'
+    
+  match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
