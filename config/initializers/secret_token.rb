8b25942c61870d358dcd2dd8d81876ceb9f48b75
mod washi <your.email@example.com>
2013-03-07 14:50:54 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrawApp::Application.config.secret_token = 'b57fe1f465567f05d8ccfab92a97aca28a43c01b6f3fbc3fef5c110a0e040ad73ab2f860915f827e3111e8d21997ddcc6de7b274c5efa9aecbd10388ca311db6'
